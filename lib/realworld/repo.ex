ba421df9dfcdef925e5a776be068d54d5bdfefb5
Egbert Pot <egbert@egbertpot.nl>
2026-01-02 18:42:27 +0100

    Fixed some errors when running `mix ash.setup`

--- lib/realworld/repo.ex
+++ lib/realworld/repo.ex
@@ -1,7 +1,21 @@
 defmodule Realworld.Repo do
   use AshPostgres.Repo, otp_app: :realworld
 
+  @impl true
   def installed_extensions do
-    ["ash-functions"]
+    # Add extensions here, and the migration generator will install them.
+    ["ash-functions", "citext"]
+  end
+
+  # Don't open unnecessary transactions
+  # will default to `false` in 4.0
+  @impl true
+  def prefer_transaction? do
+    false
+  end
+
+  @impl true
+  def min_pg_version do
+    %Version{major: 17, minor: 2, patch: 0}
   end
 end
